8fc7708d25841c1085e8f6fc78a90573b0474f2b
AxE <josephcoelo@hotmail.com>
2017-05-16 18:41:32 -0500

    Empezamos ?

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var secret_key = "My_key";
-var map_api_key="AIzaSyBoL26hVGudbJBDMo5yT3PIc-A93EX67NE";
+var map_api_key="AIzaSyAJx3oOJKCSdAJluigFP9MZLqFLl7pio_E";
 var server_domain = 'http://173.230.140.74';
 //var server_domain = 'http://192.168.1.31/shajeer/cmc_website/Source';
 //var server_domain = 'http://192.168.138.31/lidiya/callmycab';
