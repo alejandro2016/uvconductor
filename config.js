ec1d904f0c429a6fe206a4bb2bc0a49c275923d7
AxE <josephcoelo@hotmail.com>
2020-07-16 14:07:06 -0500

    2020

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var secret_key = "My_key";
 var map_api_key="AIzaSyAJx3oOJKCSdAJluigFP9MZLqFLl7pio_E";
-var server_domain = 'http://173.230.140.74';
+var server_domain = 'http://45.56.76.71';
 //var server_domain = 'http://192.168.1.31/shajeer/cmc_website/Source';
 //var server_domain = 'http://192.168.138.31/lidiya/callmycab';
 var wordpress = false;
